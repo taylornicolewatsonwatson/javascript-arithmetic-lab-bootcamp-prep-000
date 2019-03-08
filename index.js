9c3d8ac803f220346f1a9e9d485c78adbeaecbda
Taylor Watson <taylortwatson@gmail.com>
2019-03-08 23:19:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function multiply(a,b){ return a * b; }
 function divide(a,b){ return a / b;}
 function inc(n){ return n += 1; }
 function dec(n){ return n -= 1;}
-function makeInt(n) { parseInt
+function makeInt(n) { return parseInt
 function makeInt
