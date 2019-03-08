a9bd03eee672e89079824c7df131cc356b3a2047
Taylor Watson <taylortwatson@gmail.com>
2019-03-08 23:17:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ function add(a,b){ return a + b; }
 function subtract(a,b){ return a - b; }
 function multiply(a,b){ return a * b; }
 function divide(a,b){ return a / b;}
+
+function inc(n){ return number++; }
